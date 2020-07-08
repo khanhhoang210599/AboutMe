9fe5af49066c9d3575749c76081bb9cc12357dc7
khanhhoang210599 <57130524+khanhhoang210599@users.noreply.github.com>
2020-07-08 09:43:12 +0700

    Initial Commit

---
+++ AboutMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AboutMe
+//
+//  Created by admin on 5/20/20.
+//  Copyright © 2020 Khanhdepgai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
